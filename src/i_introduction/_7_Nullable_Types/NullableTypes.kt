2b5995c7428831d2aa0c11ccef63b413506bd207
Oliver Burn <oliver@puppycrawl.com>
2016-02-27 10:08:12 +1100

    recording work

--- src/i_introduction/_7_Nullable_Types/NullableTypes.kt
+++ src/i_introduction/_7_Nullable_Types/NullableTypes.kt
@@ -25,7 +25,24 @@ fun todoTask7(client: Client?, message: String?, mailer: Mailer): Nothing = TODO
 fun sendMessageToClient(
         client: Client?, message: String?, mailer: Mailer
 ) {
-    todoTask7(client, message, mailer)
+    val email: String? = client?.personalInfo?.email
+    if (email == null || message == null) return;
+
+    mailer.sendMessage(email, message)
+
+
+    /*
+            if (client == null || message == null) return;
+
+        PersonalInfo personalInfo = client.getPersonalInfo();
+        if (personalInfo == null) return;
+
+        String email = personalInfo.getEmail();
+        if (email == null) return;
+
+        mailer.sendMessage(email, message);
+
+     */
 }
 
 class Client (val personalInfo: PersonalInfo?)
