088ab4cdf89aedcac0bc44463bc45d29ca6eaabe
Oliver Burn <oliver@puppycrawl.com>
2016-03-05 21:05:55 +1100

    recording work

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,15 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override operator fun compareTo(other: MyDate) = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +17,9 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+//class DateRange(val start: MyDate, val endInclusive: MyDate) {
+//    operator fun contains(d: MyDate) = start <= d && d <= endInclusive
+//}
+
+class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>
+
